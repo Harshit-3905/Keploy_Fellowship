647bd2d75820712f96cd39d9943f6c9ff47e445a
Harshit-3905 <harshitjoshileet@gmail.com>
2023-01-26 11:59:02 +0530

    Changed Type of Jokes fetched

--- Challenge 3/index.js
+++ Challenge 3/index.js
@@ -4,11 +4,11 @@ let punchline = document.querySelector(".punchline")
 
 
 a.onclick=function(){
-    fetch(`https://official-joke-api.appspot.com/jokes/programming/random`)
+    fetch(`https://official-joke-api.appspot.com/random_joke`)
     .then((response) => response.json())
     .then((data) => {
-        const setupcontent = data[0].setup;
-        const punchlinecontent = data[0].punchline;
+        const setupcontent = data.setup;
+        const punchlinecontent = data.punchline;
         // console.log(setupcontent);
         // console.log(punchlinecontent);
         setup.innerHTML= setupcontent;
